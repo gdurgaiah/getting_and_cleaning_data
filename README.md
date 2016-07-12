90594222554d08726af18d99aa0e6f6c0f1a415a
gdurgaiah <girishdurgaiah@gmail.com>
2016-07-11 23:07:58 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+Getting and Cleaning Data: Course Project
+Introduction
+
+Goal: 
+The goal of this project is to prepare tidy data.
+
+Data:
+The data for the project is from the below URL:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+
+I created a R script called run_analysis.R that does the following.
+1. Merges the training and the test sets to create one data set.
+Read all the test and training files: y_test.txt, subject_test.txt and X_test.txt.
+Combine the files to a data frame in the form of subjects, labels, the rest of the data.We can use combine the respective data in training and test data sets corresponding to subject, activity and features. The results are stored in subject, activity and features.
+
+2. Extracts only the measurements on the mean and standard deviation for each measurement.Read the features from features.txt and filter it to only leave features that are either means or standard deviations . A new data frame is then created that includes subjects, labels and the described features.
+
+3. Uses descriptive activity names to name the activities in the data set
+Read the activity labels from activity_labels.txt and replace the numbers with the text.
+
+4. Appropriately labels the data set with descriptive activity names.
+
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.Write the new tidy set into a text file called tidy2.txt, formatted similarly to the original files.
+
+Installed Packages:
+Requires the plyr & reshape2 packages.
+
+
+About the raw data
+
+The UCI HAR Dataset must be extracted and..
+The UCI HAR Dataset must be availble in a directory called "UCI HAR Dataset"
+
+
+Lastly, the script will create a tidy data set containing the means of all the columns per test subject and per activity. This tidy dataset will be written to a tab-delimited file called tidy.txt, which can also be found in this repository.
---
+++ run_analysis.R
@@ -0,0 +1,106 @@
+## R script - run_analysis.R 
+
+## The R Script does the following steps: 
+
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names.
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## STEP 1: Merges the training and the test sets to create one tidy data set
+
+library(dplyr);
+
+setwd ("~/DataScience/Data Science Lessons/Lesson3 - ETL/Project")
+
+# Read the activity rows for test
+activitytest<-read.table("UCI HAR Dataset/test/y_test.txt", header=F)
+
+# Read the activity rows for train
+activitytrain <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+
+# Add the test and train data together
+activity<-rbind(activitytest, activitytrain)
+
+# Read the subject rows for test
+subjecttest<-read.table("UCI HAR Dataset/test/subject_test.txt", header=F)
+
+# Read the subject rows for train
+subjecttrain<-read.table("UCI HAR Dataset/train/subject_train.txt", header=F)
+
+# Add the test and train data together
+subject<-rbind(subjecttest, subjecttrain)
+
+# Create a column name
+colnames(activity)<-"Activity"
+colnames(subject)<-"Subject"
+
+# Read the featurelabels for the columns of the test and train data
+featurelabels<-read.table("UCI HAR Dataset/features.txt", header=F, sep=" ")
+colnames(featurelabels)<-c("featurenr", "featurename")
+
+# Read the x-test feature data
+featurestest<-read.table("UCI HAR Dataset/test/x_test.txt", header=F)
+
+# Read the x-train feature data
+featurestrain<-read.table("UCI HAR Dataset/train/x_train.txt", header=F)
+
+# Add the test and train data together
+features<-rbind(featurestest, featurestrain)
+
+# Create column names for the featurelabels
+colnames(features) <- make.names(featurelabels$featurename, unique=TRUE)
+
+# add the activity, subject feature data together
+data<-cbind(activity, subject, features)
+
+## STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement
+# Create a new table containing only columns with subjects, activity, mean and std
+datameanstd <- select(data,matches("Subject|Activity|mean|std"))
+
+# Check the structure
+dim(datameanstd)
+
+## STEP 3: Uses descriptive activity names to name the activities in the data set
+# Read the labels for the activities
+activitylabels<-read.table("./UCI HAR Dataset/activity_labels.txt", header=F, sep=" ")
+
+# Check the structure
+head(activitylabels)
+
+# Create column names for activity labels
+colnames(activitylabels)<- c("activityid","activity")
+
+# Add the activity label to the dataset using a merge on activityid
+data <- merge(x=datameanstd, y=activitylabels)
+
+# Exclude the activityid field
+data <- select(data, -activityid)
+
+## STEP 4: Appropriately labels the data set with descriptive
+
+## activity names.
+names(data)<-gsub("Acc", "Accelerometer", names(data))
+names(data)<-gsub("Gyro", "Gyroscope", names(data))
+names(data)<-gsub("BodyBody", "Body", names(data))
+names(data)<-gsub("Mag", "Magnitude", names(data))
+names(data)<-gsub("^t", "Time", names(data))
+names(data)<-gsub("^f", "Frequency", names(data))
+names(data)<-gsub("tBody", "TimeBody", names(data))
+names(data)<-gsub("-mean()", "Mean", names(data), ignore.case = TRUE)
+names(data)<-gsub("-std()", "STD", names(data), ignore.case = TRUE)
+names(data)<-gsub("-freq()", "Frequency", names(data), ignore.case = TRUE)
+names(data)<-gsub("angle", "Angle", names(data))
+names(data)<-gsub("gravity", "Gravity", names(data))
+
+## STEP 5: Creates a second, independent tidy data set with the
+
+## average of each variable for each activity and each subject.
+library(plyr);
+
+tdata<-aggregate(. ~Subject + activity, data, mean)
+
+tdata<-tdata[order(tdata$Subject,tdata$activity),]
+
+write.table(tdata, file = "tidy_data.txt",row.name=FALSE)
